9e0d0b7cfe0d7ea98b8e8c939f7f7b03418b0c36
QuanGhu <ariteknologi@gmail.com>
2018-05-12 15:38:04 +0700

    Add : link admin on footer

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -8,6 +8,7 @@ export default class footer extends React.Component {
                     <div className="container">
                         <div className="footer-inner">
                             <div className="copyright">© Data Set, All rights reserved</div>
+                            <div className="pull-right"><a href="http://185.201.8.164:3000/" target="__blank">Admin</a></div>
                         </div>
                     </div>
                 </section>
